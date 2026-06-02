9d0ad1d656203f04a234e1ed370adb98301e084f
Rangon Roy Utsab <rangonroy@outlook.com>
2026-06-02 16:32:35 +0600

    fix: swagger basepath

--- controllers/auth.go
+++ controllers/auth.go
@@ -23,7 +23,7 @@ type AuthController struct {
 // @Failure 400 {object} controllers.BasicResponse
 // @Failure 409 {object} controllers.BasicResponse
 // @Failure 500 {object} controllers.BasicResponse
-// @router /api/v1/auth/register [post]
+// @router /auth/register [post]
 func (c *AuthController) Register() {
 	var request RegisterRequest
 
@@ -89,7 +89,7 @@ func (c *AuthController) Register() {
 // @Failure 400 {object} controllers.BasicResponse
 // @Failure 401 {object} controllers.BasicResponse
 // @Failure 500 {object} controllers.BasicResponse
-// @router /api/v1/auth/login [post]
+// @router /auth/login [post]
 func (c *AuthController) Login() {
 	var request LoginRequest
 
